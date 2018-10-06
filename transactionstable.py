9a002f425e245b5f5c52ae3808fe46c2b68d57c6
myth <mugadzatt001@gmail.com>
2018-10-06 21:55:41 +0200

    tables test

--- transactionstable.py
+++ transactionstable.py
@@ -3,7 +3,8 @@ from querybuilder import QueryBuilder
 class Ttable(object):
 	def __init__(self):
 		self._q = QueryBuilder()
-		self._c = ["reference","creference","account","amount"]
+		self._c = ["reference","creference","account","transactionstatus"
+		,"amount"]
 		self._t = "transactions"
 		
 	def _columns(self):
@@ -12,7 +13,8 @@ class Ttable(object):
 		columns.append(self._q.column_(self._c[0]).string_().build_())
 		columns.append(self._q.column_(self._c[1]).string_().build_())
 		columns.append(self._q.column_(self._c[2]).real_().build_())
-		columns.append(self._q.column_(self._c[3]).real_().not_null().build_())
+		columns.append(self._q.column_(self._c[3]).string_(2).build_())
+		columns.append(self._q.column_(self._c[4]).real_().not_null().build_())
 		return columns
 		
 	def	table_create(self):
